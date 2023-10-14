761c096f34e771d69dfefbabc16c0cfc4931934c
Aiko <tysaiw@gmail.com>
2023-10-14 20:24:06 +0800

    add controller todos

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,37 @@
+class TodosController < ApplicationController
+  before_action :set_todo, except: [:index]
+
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo.update(params_todo)
+      redirect_to @todo, notice: 'Todo was successfully updated.'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @todo.destroy
+    redirect_to todos_url, notice: 'Todo was successfully destroyed.'
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find_by(id: params[:id])
+    redirect_to todos_path, alert: "Todo not found" unless @todo
+  end
+
+  def params_todo
+    params.require(:todo).permit(:title, :content, :status, :due_date, :start_at, :end_at)
+  end
+end
